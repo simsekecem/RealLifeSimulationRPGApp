d9de474b050b7eb896e3574ee976d20abff82cc1
simsekecem <simsekecem98@gmail.com>
2025-11-29 22:45:14 +0300

    Add XP label to missions and update UI layout

--- scenes/UserInterface/MissionsWindow.gd
+++ scenes/UserInterface/MissionsWindow.gd
@@ -6,16 +6,16 @@ extends Control
 func _ready():
 	# Örnek 10 mission
 	var example_missions = [
-		{"name":"Mission 1", "detail":"Detail for mission 1"},
-		{"name":"Mission 2", "detail":"Detail for mission 2"},
-		{"name":"Mission 3", "detail":"Detail for mission 3"},
-		{"name":"Mission 4", "detail":"Detail for mission 4"},
-		{"name":"Mission 5", "detail":"Detail for mission 5"},
-		{"name":"Mission 6", "detail":"Detail for mission 6"},
-		{"name":"Mission 7", "detail":"Detail for mission 7"},
-		{"name":"Mission 8", "detail":"Detail for mission 8"},
-		{"name":"Mission 9", "detail":"Detail for mission 9"},
-		{"name":"Mission 10", "detail":"Detail for missiondsfsdfdsfdsffffffdkjsakjjKDKJLASJLFKJSDKLFKJSDJGKSKJLDFKJFSJGKSDKJFDSFKJUEIKJSDFK SFOJSAKJD SJDFJKDSJKF SDJFJDSKKJF SDKJFKJDSF ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff 10"}
+		{"name":"Mission 1", "detail":"Detail for mission 1", "XP":"10 XP"},
+		{"name":"Mission 2", "detail":"Detail for mission 2", "XP":"10"},
+		{"name":"Mission 3", "detail":"Detail for mission 3", "XP":"10"},
+		{"name":"Mission 4", "detail":"Detail for mission 4", "XP":"10"},
+		{"name":"Mission 5", "detail":"Detail for mission 5", "XP":"1140 XP"},
+		{"name":"Mission 6", "detail":"Detail for mission 6", "XP":"1230"},
+		{"name":"Mission 7", "detail":"Detail for mission 7", "XP":"1024"},
+		{"name":"Mission 8", "detail":"Detail for mission 8", "XP":"1240"},
+		{"name":"Mission 9", "detail":"Detail for mission 9", "XP":"1120"},
+		{"name":"Mission 10", "detail":"Detail for missiondsfsdfdsfdsffffffdkjsakjjKDKJLASJLFKJSDKLFKJSDJGKSKJLDFKJFSJGKSDKJFDSFKJUEIKJSDFK SFOJSAKJD SJDFJKDSJKF SDJFJDSKKJF SDKJFKJDSF ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff 10", "XP":"1120"}
 	]
 	load_missions(example_missions)
 
@@ -28,4 +28,5 @@ func load_missions(missions: Array):
 		var row = mission_row_scene.instantiate()
 		row.get_node("MissionList/MissionName").text = mission.name
 		row.get_node("MissionList/MissionDetail").text = mission.detail
+		row.get_node("MissionList/XPLabel").text = mission.XP
 		missions_list.add_child(row)
