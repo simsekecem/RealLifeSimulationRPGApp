033b1c5dd84c650c713e78aaafdf3b0b2df1f81c
NidaElvinMertoglu <nidaelvin16@gmail.com>
2025-12-09 13:25:55 +0300

    calender made

---
+++ scenes/prefabs/note_pop_up.gd
@@ -0,0 +1,25 @@
+extends PopupPanel
+
+@onready var title_label = $MarginContainer/VBoxContainer/TitleLabel
+@onready var text_edit = $MarginContainer/VBoxContainer/TextEdit
+@onready var exit_button = $MarginContainer/VBoxContainer/HBoxContainer/ExitBtn
+
+var current_date
+
+func _ready():
+	exit_button.pressed.connect(_on_exit_pressed)
+
+func open_for_date(date):
+	current_date = date
+	title_label.text = "%d.%d.%d" % [date.day, date.month, date.year]
+	text_edit.text = ""
+
+	popup_centered()
+
+	# 🔥 BURASI ZORUNLU
+	await get_tree().process_frame
+	text_edit.grab_focus()
+
+
+func _on_exit_pressed():
+	hide()
